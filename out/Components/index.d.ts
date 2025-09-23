35c64720ffee0affa91f58108d0a10b71c24be5c
Jack Taylor <jack@jackt.space>
2025-09-23 19:25:02 -0400

    Remove out folder

--- out/Components/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type UltimateScrollingFrame from "./ScrollingFrame";
-
-declare const Components: {
-	ScrollingFrame: typeof UltimateScrollingFrame;
-};
-
-export = Components;
--- out/Renderers/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type Renderer<T> =
-	| {
-			type: "byState";
-			callback: (value: T) => React.ReactNode;
-	  }
-	| {
-			type: "byBinding";
-			callback: (binding: React.Binding<T | undefined>) => React.ReactNode;
-	  };
